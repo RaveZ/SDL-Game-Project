01ba9b93ad710055f82da752ec81cdffc5a2b6a9
Ivan <75530916+RaveZ@users.noreply.github.com>
2026-08-11 12:36:28 +0700

    init commit

---
+++ SDL_Game.cpp
@@ -0,0 +1,12 @@
+﻿// SDL_Game.cpp : Defines the entry point for the application.
+//
+
+#include "SDL_Game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
