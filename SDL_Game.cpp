ca51f4ba1d59184dcb3120e63cb8e1a9533a726e
Ivan <75530916+RaveZ@users.noreply.github.com>
2026-09-24 15:53:12 +0700

    update add asset

--- SDL_Game.cpp
+++ SDL_Game.cpp
@@ -1,12 +1,80 @@
-﻿// SDL_Game.cpp : Defines the entry point for the application.
-//
-
-#include "SDL_Game.h"
+﻿#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 using namespace std;
 
-int main()
+struct SDLState
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	SDL_Window* window;
+    SDL_Renderer* renderer;
+};
+
+void cleanup(SDLState& state);
+
+int main(int argc, char* argv[])
+{
+    SDLState state; 
+
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
+        return 1;
+    }
+
+    // Create window
+    int width = 800;
+    int height = 600;
+
+    state.window = SDL_CreateWindow("SDL3 Demo", width, height, 0);
+    
+    if (!state.window)
+    {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window", nullptr);
+        cleanup(state);
+        return 1;
+    }
+
+    //create the renderer
+    state.renderer = SDL_CreateRenderer(state.window, nullptr);
+    if (!state.renderer)
+    {
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating renderer", nullptr);
+        cleanup(state);
+		return 1;
+    }
+
+
+
+    //start the game loop
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event{0};
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+                break;
+            }
+        }
+
+		SDL_SetRenderDrawColor(state.renderer, 0, 0, 255, 255);
+		SDL_RenderClear(state.renderer);
+
+        //swap buffer and present
+		SDL_RenderPresent(state.renderer);
+
+    }
+
+    // Cleanup and quit
+    cleanup(state);
+    return 0;
+}
+
+void cleanup(SDLState &state)
+{
+	SDL_DestroyRenderer(state.renderer);
+    SDL_DestroyWindow(state.window);
+    SDL_Quit();
 }
